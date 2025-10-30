21067ab741fdd7395199701366b63cbc70929f8a
wwc-66 <2861681553@qq.com>
2025-10-30 16:44:40 +0800

    课程代码微调完善

--- W7，8_排序与查找/归并排序.py
+++ W7，8_排序与查找/归并排序.py
@@ -39,18 +39,23 @@ def mergeSort(alist):
 def merge_sort(lst):
     if len(lst) <= 1:
         return lst
-
+    #取中点
     middle = len(lst)//2
+    #对左右子列表进行递归调用，使左右两部分排好顺序
     left = merge_sort(lst[:middle])
     right = merge_sort(lst[middle:])
 
+    #初始化合并列表
     merged = []
+    #当左右子表内同时还有元素时
     while left and right:
+        #如果左子表第一位元素小于右子表第一位元素
         if left[0] < right[0]:
-            merged.append(left[0])
-
+            #合并列表添加左子表第一位元素,同时删去这个元素
+            merged.append(left.pop(0))
+        #反之，添加右子列表第一位元素并在右子列表中删去该元素
         else:
-            merged.append(right[0])
-
+            merged.append(right.pop(0))
+    #将左右子表剩余的元素加到合并列表的末尾
     merged.extend(right if right else left)
     return merged
